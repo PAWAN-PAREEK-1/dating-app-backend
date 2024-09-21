4ac3cb331216d1ffe692a1acd0b2f19c59641f20
pawan-pareek-1 <pawapareek506@gmail.com>
2024-09-21 14:01:02 +0530

    reset password

--- controller/user/authcontroller.js
+++ controller/user/authcontroller.js
@@ -1,9 +1,11 @@
 import bcrypt from "bcryptjs";
 import { validationResult } from "express-validator";
 import User from "../../models/user.js"; // Adjust the path to your User model
-import jwt from 'jsonwebtoken';
-import { genrateVerificationToken,sendVerficationEmail } from "./verificationController.js";
-
+import jwt from "jsonwebtoken";
+import {
+  genrateVerificationToken,
+  sendVerficationEmail,
+} from "./verificationController.js";
 
 export const registerUser = async (req, res) => {
   // Check for validation errors
@@ -12,91 +14,130 @@ export const registerUser = async (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { username, email, password, gender, dateOfBirth ,name ,mobile} = req.body;
+  const { username, email, password, gender, dateOfBirth, name, mobile } =
+    req.body;
 
   try {
-
     // const findUser = await User.findOne({email})
     // if(findUser){
     //     return res.status(200).json({message: "user already registered"})
     // }
 
-
-    const salt= await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt);
-    console.log("user api authenticated")
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("user api authenticated");
     const user = new User({
-        name:name,
-        mobile:mobile,
-        username:username,
-        email:email,
-        password:hashedPassword,
-        gender:gender,
-        dateOfBirth:dateOfBirth
-
-    })
-
-
-    await user.save()
-
-
-
-    const verificationToken = genrateVerificationToken(user.id,user.email)
-
-
-
-    sendVerficationEmail(user,verificationToken)
-
-    res.status(201).json({
-        success:true,
-        message: 'User registered successfully. Please check your email to verify your account.',
-        user: {
-            id: user._id,
-            username: user.username,
-            email: user.email
-        }
+      name: name,
+      mobile: mobile,
+      username: username,
+      email: email,
+      password: hashedPassword,
+      gender: gender,
+      dateOfBirth: dateOfBirth,
     });
 
+    await user.save();
 
+    const verificationToken = genrateVerificationToken(user.id, user.email);
+
+    sendVerficationEmail(user, verificationToken);
+
+    res.status(201).json({
+      success: true,
+      message:
+        "User registered successfully. Please check your email to verify your account.",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({success:false, message: "Server error" });
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
 export const login = async (req, res) => {
-    const {email, password} = req.body;
-    console.log(email, password);
+  const { email, password } = req.body;
+  console.log(email, password);
 
-    try {
+  try {
+    const user = await User.findOne({ email });
 
-        const user= await User.findOne({email});
-
-        if (!user){
-            return res.status(404).json({ message:"invalid access"});
-        }
-
-        const checkPassword = await bcrypt.compare(password,user.password)
-        if(!checkPassword){
-            return res.status(404).json({ message:"invalid access"});
-        }
-
-        const token = jwt.sign(
-            { id: user._id, username: user.username },
-            process.env.JWT_SECRET,
-            { expiresIn: '111h' }  // Token expires in 1 hour
-        );
-
-        res.status(200).json({
-            success:true,
-            message: 'Login successful',
-            token
-        });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({success:false, message: 'Server error' });
+    if (!user) {
+      return res.status(404).json({ message: "invalid access" });
     }
-}
 
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+      return res.status(404).json({ message: "invalid access" });
+    }
 
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "111h" } // Token expires in 1 hour
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+  const userId = req.user.id;
+
+  try {
+    const checkUser = await User.findById(userId);
+
+    if (!checkUser || !currentPassword || !newPassword) {
+      return res
+        .status(404)
+        .json({
+          success: false,
+          message: "issue to find the user or may be some fields are missing",
+        });
+    }
+
+    if (currentPassword === newPassword) {
+      return res
+        .status(200)
+        .json({
+          success: false,
+          message: "password cannot be same with old password",
+        });
+    }
+
+    const compare = await bcrypt.compare(currentPassword, checkUser.password);
+
+    if (!compare) {
+      return res
+        .status(404)
+        .json({ success: false, message: "password mismatch" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+    checkUser.password = hashedPassword;
+
+    await checkUser.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Password reset successfully" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "issue in reseting password" });
+  }
+};
--- routes/User/authRoutes.js
+++ routes/User/authRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { login, registerUser } from '../../controller/user/authcontroller.js';
+import { login, registerUser, resetPassword } from '../../controller/user/authcontroller.js';
 import { userEmailAndMobileCheck, userNameCheck } from '../../middelware/checkUsername.js';
 import { verifyEmail } from '../../controller/user/verificationController.js';
+import { authMiddleware } from '../../middelware/tokenValidate.js';
 
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.route('/register').post(userEmailAndMobileCheck,userNameCheck,registerUser);
 router.route('/login').post(login);
 router.route('/verify').post(verifyEmail);
+router.route('/reset').post(authMiddleware,resetPassword);
 
 export {router as auth}
